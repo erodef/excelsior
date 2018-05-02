3a5c5c0d780eb012a00c209e8168a61556699369
arcerix <rafael.avilar1@gmail.com>
2018-05-02 12:26:54 -0300

    Starting prep work for the next updates.

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 # coding=utf-8
 """
-    TODO: More monsters, skills
-    TODO: 1 more design pattern(decorators?)
-
     TODO: -EXTRA- Battle visuals (hit numbers, hit marks, warnings, different monster sprites etc)
-    TODO: -EXTRA- Proper monster attacks and AI
     TODO: -EXTRA- Choices during dungeon movement
-    TODO: -EXTRA- Combos
-    TODO: -EXTRA- Music and sfx
 """
 import json
 import random
@@ -15,7 +9,7 @@ import tdl
 import sys
 import copy
 from components.enemy_ai import *
-from game_states import *
+from engine_combat import *
 from engine import *
 from rendering import *
 from logbox import *
@@ -26,87 +20,76 @@ from colors import getColors
 
 
 def main():
-
-    Game = Engine()
-    colors = Game.colors
+    G = Engine()
+    colors = G.colors
     mact_counter = 0
     menu_cursor = 0
 
     # TDL init
-    tdl.set_font(Game.font, greyscale=Game.greyscale, altLayout=Game.altLayout)
-    root_console = tdl.init(Game.screen_width, Game.screen_height, title=Game.title)
+    tdl.set_font(G.font, greyscale=G.greyscale, altLayout=G.altLayout)
+    root_console = tdl.init(G.screen_width, G.screen_height, title=G.title)
     tdl.set_fps(30)
 
     # Console
-    con = tdl.Console(Game.screen_width, Game.screen_height)
-
-    # UI Panel
-    hud = tdl.Console(Game.screen_width, Game.screen_height)
-
-    # Message Log
-    message_log = MessageLog(Game.message_x, Game.message_width, Game.message_height)
-
+    con = tdl.Console(G.screen_width, G.screen_height)
 
     # Player init
-    pc = Game.return_player()
+    pc = G.return_player()
 
-    if Game.starting:
-        gamestate = State.MENU
-        Game.gen_dungeon(Game.dungeon_levels)
-        Game.starting = False
+    if not G.started:
+        G.state = State.MENU
+        G.gen_dungeon(G.dungeon_levels)
+        G.started = True
 
     # Draw
     while not tdl.event.is_window_closed():
         con.clear()
 
         # Drawing on screen in menu
-        if gamestate == State.MENU:
-            draw_menu(con, Game)
+        if G.state == State.MENU:
+            draw_menu(con, G)
 
         # Drawing on screen in room
-        if gamestate == State.ROOM_PHASE:
-            draw_room(con, Game)
+        if G.state == State.ROOM_PHASE:
+            draw_room(con, G)
 
         # Drawing on screen during battle
-        if gamestate == State.BATTLE_PHASE:
-            draw_battle(con, Game, pc)
-
-            root_console.blit(con, 0, 0, Game.screen_width, Game.screen_height, 0, 0)
+        if G.state == State.BATTLE_PHASE:
+            draw_battle(con, G, pc)
+            root_console.blit(con, 0, 0, G.screen_width, G.screen_height, 0, 0)
 
         # Drawing on screen after battle
-        if gamestate == State.UPGD_PHASE:
-            draw_upgd(con, Game, pc)
+        if G.state == State.UPGD_PHASE:
+            draw_upgd(con, G, pc)
 
-        if gamestate == State.ENDING:
-            tmd = "You WIN!"
-            con.draw_str(int(Game.screen_width/2)-int(len(tmd)/2),int(Game.screen_height/2)-5, tmd)
-            kms = "Press Enter to continue..."
-            con.draw_str(int(Game.screen_width/2)-int(len(kms)/2),int(Game.screen_height/2)-4, kms)
+        # Drawing on screen on game ending phase
+        if G.state == State.ENDING:
+            draw_ending(con, G)
 
-        root_console.blit(con, 0, 0, Game.screen_width, Game.screen_height, 0, 0)
+        root_console.blit(con, 0, 0, G.screen_width, G.screen_height, 0, 0)
         tdl.flush()
 
-        Game.mousedown = False
         # Handle events
+        G.mousedown = False
         for event in tdl.event.get():
             if event.type == 'KEYDOWN':
                 user_input = event
                 break
             elif event.type == 'MOUSEMOTION':
-                Game.mouse = event.cell
-                Game.mouse_x, Game.mouse_y = Game.mouse
+                G.mouse = event.cell
+                G.mouse_x, G.mouse_y = G.mouse
             if event.type == 'MOUSEDOWN':
-                Game.mousedown = True
-
+                G.mousedown = True
         # If no input matches KEYDOWN, set user_input to None
         else:
             user_input = None
 
-        # Handling events in diferent gamestates
-        if gamestate == State.BATTLE_PHASE:
-            enemy = Game.current_level.entity
+        # Handling events in different G.states
+        if G.state == State.BATTLE_PHASE:
+            enemy = G.current_level.entity
             results = []
-            if Game.combat_locked:
+
+            if G.combat_locked:
                 # Player
                 for skill in pc.skills:
                     if skill.timeout < skill.max_timeout:
@@ -121,7 +104,7 @@ def main():
                 # Handles user attack inputs
                 if user_input:
                     if user_input.keychar == '1' or user_input.keychar == '2' or user_input.keychar == '3' or user_input.keychar == '4':
-                        skill = pc.skills[int(user_input.keychar)-1]
+                        skill = pc.skills[int(user_input.keychar) - 1]
                         if skill.timeout == skill.max_timeout:
                             results = skill.activate(pc, enemy)
 
@@ -147,70 +130,68 @@ def main():
 
                 # Handling results
                 for result in results:
-                    message = result.get('message')
                     dead_entity = result.get('dead')
-                    if message:
-                        message_log.add_message(message)
 
                     if dead_entity:
-                        Game.combat_locked = False
+                        G.combat_locked = False
                         if dead_entity != pc:
                             mact_counter = 0
-                            pc.absorb(1)
-                            Game.endtext = Message("Victory!", colors.get('green'))
+                            pc.level_up_by(1)
+                            G.endtext = Message("Victory!", colors.get('green'))
 
                         elif dead_entity == pc:
                             mact_counter = 0
-                            Game.endtext = Message("DEAD!", colors.get('red'))
-                            Game.gameover = True
+                            G.endtext = Message("DEAD!", colors.get('red'))
+                            G.over = True
 
-            if not Game.combat_locked:
+            if not G.combat_locked:
                 if user_input and user_input.key == 'ENTER':
-                    if Game.gameover == False:
+                    if not G.over:
                         for skill in pc.skills:
                             skill.timeout = skill.max_timeout
-                        message_log.messages = []
-                        if Game.current_level == Game.dungeon[-1]:
-                            gamestate = State.ENDING
+                        if G.current_level == G.dungeon[-1]:
+                            G.state = State.ENDING
                         else:
-                            gamestate = State.UPGD_PHASE
+                            G.state = State.UPGD_PHASE
+                        print("go")
                     else:
-                        Game.gameover = False
-                        Game.reset()
-                        pc = Game.return_player()
-                        gamestate = State.MENU
+                        G.over = False
+                        G.reset()
+                        pc = G.return_player()
+                        G.state = State.MENU
 
-        elif gamestate == State.ROOM_PHASE:
+        elif G.state == State.ROOM_PHASE:
             if user_input and user_input.key == 'ENTER':
-                if Game.current_level.entity:
-                    gamestate = State.BATTLE_PHASE
-                    Game.combat_locked = True
+                if G.current_level.entity:
+                    G.state = State.BATTLE_PHASE
+                    G.combat_locked = True
 
-        elif gamestate == State.UPGD_PHASE:
+        elif G.state == State.UPGD_PHASE:
             if user_input and user_input.key == 'ENTER':
-                if Game.upgd_selec:
-                    pc.upgd_skillset(Game.upgd_selec)
-                    Game.upgd_selec = ''
-                    Game.next_level()
-                    gamestate = State.ROOM_PHASE
+                if G.upgd_selec:
+                    pc.upgd_skillset(G.upgd_selec)
+                    G.upgd_selec = ''
+                    G.next_level()
+                    G.state = State.ROOM_PHASE
 
-        elif gamestate == State.MENU:
+        elif G.state == State.MENU:
             if user_input and user_input.key == 'ENTER':
-                gamestate = State.ROOM_PHASE
+                G.state = State.ROOM_PHASE
 
-        elif gamestate == State.ENDING:
+        elif G.state == State.ENDING:
             if user_input and user_input.key == 'ENTER':
-                Game.gameover = False
-                Game.reset()
-                pc = Game.return_player()
-                gamestate = State.MENU
+                G.over = False
+                G.reset()
+                pc = G.return_player()
+                G.state = State.MENU
 
         # Controls
-        if user_input and user_input.key == 'ESCAPE' :
+        if user_input and user_input.key == 'ESCAPE':
             return True
 
-        if user_input and user_input.key == 'ENTER' and user_input.alt :
+        if user_input and user_input.key == 'ENTER' and user_input.alt:
             tdl.set_fullscreen(not tdl.get_fullscreen())
+
 
 if __name__ == '__main__':
     main()
--- pc.py
+++ pc.py
@@ -8,9 +8,9 @@ class PC(Entity):
     """
     def __init__(self, name, fighter):
         super().__init__(name, fighter=fighter)
-        self.soulstack = 0
+        self.points = 0
 
-    def absorb(self, s):
+    def level_up_by(self, s):
         self.max_hp += int(self.max_hp*0.2)
         self.hp = self.max_hp
-        self.soulstack += s
+        self.points += s
